1660fb7fab50d6b58f35ede315568ac97745e681
Kshitij Goel <68154931+Goelksh@users.noreply.github.com>
2022-06-24 08:16:01 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,227 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Sad)
+    soundExpression.sad.playUntilDone()
+    sleepSequence()
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Heart)
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.happy), SoundExpressionPlayMode.InBackground)
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    sleepSequence()
+})
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showIcon(IconNames.Happy)
+    soundExpression.giggle.playUntilDone()
+    sleepSequence()
+})
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showIcon(IconNames.Happy)
+    soundExpression.giggle.playUntilDone()
+    sleepSequence()
+})
+function sleepSequence () {
+    basic.pause(500)
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.yawn), SoundExpressionPlayMode.InBackground)
+    basic.pause(750)
+    basic.showLeds(`
+        . . . . .
+        . . . # #
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+    basic.showIcon(IconNames.Asleep)
+}
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.happy), SoundExpressionPlayMode.InBackground)
+    basic.showLeds(`
+        # # . # #
+        # # . # #
+        . . . . .
+        . # . # .
+        . # # # .
+        `)
+    sleepSequence()
+})
+input.onSound(DetectedSound.Loud, function () {
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.sad), SoundExpressionPlayMode.UntilDone)
+    basic.showLeds(`
+        . . . . .
+        # # . # #
+        . . . . .
+        . # # # .
+        # . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # . # #
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . #
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        # . # . .
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # . # .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . . . #
+        . . . . .
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . # . #
+        . . . . .
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        # . # . #
+        . . . . .
+        . # # # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        # . # . #
+        . . . . .
+        . # # # .
+        `)
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.soaring), SoundExpressionPlayMode.UntilDone)
+    basic.pause(1000)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        # . . . #
+        . . . . .
+        . # # # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        # . . . #
+        . . . . .
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . . . #
+        . . . . .
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.yawn), SoundExpressionPlayMode.UntilDone)
+    basic.showLeds(`
+        . . . . .
+        # # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        # # . # #
+        . . . . .
+        . # # # .
+        # . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        # # . # #
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+})
+basic.showIcon(IconNames.Asleep)
+input.setSoundThreshold(SoundThreshold.Loud, 200)
