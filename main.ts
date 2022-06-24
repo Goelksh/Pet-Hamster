06307ecf173921d17862c207fa3de8b96bf9110e
Kshitij Goel <68154931+Goelksh@users.noreply.github.com>
2022-06-24 08:27:53 +0530

    volume reduced further

--- main.ts
+++ main.ts
@@ -225,4 +225,4 @@ input.onSound(DetectedSound.Loud, function () {
 })
 basic.showIcon(IconNames.Asleep)
 input.setSoundThreshold(SoundThreshold.Loud, 255)
-music.setVolume(100)
+music.setVolume(80)
